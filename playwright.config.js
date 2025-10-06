9553e8d3afffa7aa667a6d24671ca8679c5b18e3
marcelo <marcheloortuo@gmail.com>
2025-10-06 12:38:07 -0400

    MOC (solucionando problema git actoins con el .env)

--- playwright.config.js
+++ playwright.config.js
@@ -21,10 +21,6 @@ export default defineConfig({
     headless: process.env.HEADLESS !== "true",
     trace: "on-first-retry",
     permissions: ['clipboard-read', 'clipboard-write'],
-    //para el actions?
-    actionTimeout: 15000,  // para cada acción individual
-    navigationTimeout: 30000, // para navegaciones lentas
-    viewport: { width: 1280, height: 720 },
   },
   projects: [
     {
