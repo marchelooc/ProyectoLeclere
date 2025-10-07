94f6ecbd32d56fc4a89d9e36b392697533dea76e
miguelangel8888 <mkalyu81@gmail.com>
2025-10-07 18:00:05 -0400

    MAY: modificando todo los test cases

--- pages/confListPage.js
+++ pages/confListPage.js
@@ -1,44 +1,58 @@
-import { expect } from '@playwright/test';
+import { expect } from "@playwright/test";
 
 export class ListPage {
   constructor(page, listName) {
     this.page = page;
     this.listName = listName;
 
-    this.header = page.locator('[data-testid="list-header"]', { hasText: listName });
-    this.contraerButton=this.header.locator('button[data-testid="list-collapse-button"]');
-    this.expandirButton=this.header.locator('button[data-testid="list-collapse-button"]');    
-    this.menuButton = this.header.locator('button[data-testid="list-edit-menu-button"]'); 
+    this.header = page.locator('[data-testid="list-header"]', {
+      hasText: listName,
+    });
+    this.contraerButton = this.header.locator(
+      'button[data-testid="list-collapse-button"]'
+    );
+    this.expandirButton = this.header.locator(
+      'button[data-testid="list-collapse-button"]'
+    );
+    this.menuButton = this.header.locator(
+      'button[data-testid="list-edit-menu-button"]'
+    );
     this.botonColor = this.page.locator('button[data-testid="color-tile-red"]');
-    this.archiveButton=this.page.locator('button[data-testid="list-actions-archive-list-button"]');
-    this.seguirButton=this.page.locator('button[data-testid="list-actions-watch-list-button"]');
-    this.dejarSeguirButton=this.page.locator('button[data-testid="list-actions-unwatch-list-button"]');
-    this.quitarColorButtton=this.page.locator('button:has([data-testid="CloseIcon"])');
-    this.moverListaButton=this.page.locator('button[data-testid="list-actions-move-list-button"]');
-    this.moverButton=this.page.locator('button[type="submit"]')
-    }
+    this.archiveButton = this.page.locator(
+      'button[data-testid="list-actions-archive-list-button"]'
+    );
+    this.seguirButton = this.page.locator(
+      'button[data-testid="list-actions-watch-list-button"]'
+    );
+    this.dejarSeguirButton = this.page.locator(
+      'button[data-testid="list-actions-unwatch-list-button"]'
+    );
+    this.quitarColorButtton = this.page.locator(
+      'button:has([data-testid="CloseIcon"])'
+    );
+    this.moverListaButton = this.page.locator(
+      'button[data-testid="list-actions-move-list-button"]'
+    );
+    this.moverButton = this.page.locator('button[type="submit"]');
+  }
 
-
-    async archive() {
-        await this.page.waitForTimeout(1000);
-        await this.menuButton.click();
-        await this.archiveButton.waitFor({ state: "visible" });
-        await this.archiveButton.click();
-        await expect(this.header).toHaveCount(0);
-    }
-
-    async changeColor(color) {
-    
-    
+  async archive() {
+    await this.page.waitForTimeout(1000);
     await this.menuButton.click();
+    await this.archiveButton.waitFor({ state: "visible", timeout: 5000 });
+    await this.archiveButton.click();
+    await expect(this.header).toHaveCount(0);
+  }
+
+  async changeColor(color) {
     await this.menuButton.click();
-    const botonColor = this.page.locator(`button[data-testid="color-tile-${color}"]`);
+    const botonColor = this.page.locator(`button[data-testid=color-tile-${color}"]`);
     await this.botonColor.waitFor({ state: "visible" });
     await botonColor.click();
     await this.menuButton.click();
 }
 
-/** 
+  /** 
   async changeColor() {
     await this.menuButton.dblclick();
     await this.menuButton.click();
@@ -48,50 +62,47 @@ export class ListPage {
     //await this.CerrarMenu.click();
 }*/
 
-    async cerrarPageOconf() {
-        await this.CerrarMenu.click();
-    }
+  async cerrarPageOconf() {
+    await this.CerrarMenu.click();
+  }
 
-    async contraerLista(){
+  async contraerLista() {
     await this.contraerButton.waitFor({ state: "visible" });
     await this.contraerButton.click();
     await this.page.waitForTimeout(2000);
-    }
+  }
 
-    async seguirLista(){
-    await this.menuButton.dblclick(); 
+  async seguirLista() {
+    await this.menuButton.dblclick();
     await this.menuButton.click();
     await this.seguirButton.click();
     await this.menuButton.click();
-    }
+  }
 
-    async dejarSeguirLista(){
-    
+  async dejarSeguirLista() {
     await this.menuButton.click();
     await this.seguirButton.waitFor({ state: "visible" });
     await this.seguirButton.click();
     await this.menuButton.click();
-    }
-    async quitarColorLista(){
-    
+  }
+  async quitarColorLista() {
     await this.menuButton.click();
-    await this.quitarColorButtton.waitFor({ state: "visible" });
-    await this.quitarColorButtton.click(); 
+    await this.quitarColorButtton.click();
     await this.menuButton.click();
-    }
+  }
 
-    async moverLista(){
-
+  async moverLista() {
     await this.menuButton.click();
     await this.menuButton.click();
     await this.moverListaButton.click();
-    await this.page.locator('input[id="move-list-screen-position-select"]').fill('1');
-    await this.page.keyboard.press('Enter');
+    await this.page
+      .locator('input[id="move-list-screen-position-select"]')
+      .fill("1");
+    await this.page.keyboard.press("Enter");
     await this.moverButton.click();
-    
-    }
-    async expandirLista(){
+  }
+  async expandirLista() {
     await this.expandirButton.click();
     await this.expandirButton.click();
-    }
+  }
 }
--- tests/ui/functionList.spec.js
+++ tests/ui/functionList.spec.js
@@ -1,102 +1,120 @@
 import { test, expect } from "../../utils/fixtures.js";
-import colorsData from '../../data/colores.json';
+import colorsData from "../../data/colores.json";
 import { BoardPage } from "../../pages/boardPage.js";
 import { TableroPage } from "../../pages/tableListPage.js";
-import { ListPage} from "../../pages/confListPage.js";
+import { ListPage } from "../../pages/confListPage.js";
 import { screenshotPath } from "../../utils/helpers.js";
 import { Logger } from "../../utils/helpers.js";
 
-test("@ui @smoke Crear una lista en un tablero con letras", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {  
-    Logger.info("");
+test("@ui @smoke Crear una lista en un tablero con letras", async ({
+  loginFixture,
+}) => {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
     await boardPage.clickBotonTablero();
     Logger.info("Agregando una lista con letras");
-    await tableroPage.agregarLista('Lista de pruebas agregar');
+    await tableroPage.agregarLista("Lista de pruebas agregar");
     Logger.info("verificando que se creo en la tabla correcta");
     const confListaPage = new ListPage(page, "Lista de pruebas agregar");
-    await expect(page).toHaveURL("https://trello.com/b/wWp9CEFf/mi-tablero-de-trello");
+    await expect(page).toHaveURL(
+      "https://trello.com/b/wWp9CEFf/mi-tablero-de-trello"
+    );
     Logger.info("Eliminando la lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Crear una lista en un tablero con letras`) });
+  } catch (err) {
+    await page.screenshot({
+      path: screenshotPath("Crear una lista en un tablero con letras"),
+    });
+    Logger.error(err);
     throw err;
-    Logger.error(err);  
-    }
-        
+    
+  }
 });
 
-test("@ui @positive Crear una lista en un tablero con caracteres especiales", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+test("@ui @positive Crear una lista en un tablero con caracteres especiales", async ({
+  loginFixture,
+}) => {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
     await boardPage.clickBotonTablero();
     Logger.info("Agregando una lista con caracteres especiales");
-    await tableroPage.agregarLista('@#%$#%#^$%^$');
+    await tableroPage.agregarLista("@#%$#%#^$%^$");
     const confListaPage = new ListPage(page, "@#%$#%#^$%^$");
     Logger.info("verificando que se creo en la tabla correcta");
-    await expect(page).toHaveURL("https://trello.com/b/wWp9CEFf/mi-tablero-de-trello");
+    await expect(page).toHaveURL(
+      "https://trello.com/b/wWp9CEFf/mi-tablero-de-trello"
+    );
     Logger.info("Eliminando la lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Crear una lista en un tablero con caracteres especiales`) });
-    throw err;
+  } catch (err) {
+    await page.screenshot({
+      path: screenshotPath(
+        "Crear una lista en un tablero con caracteres especiales"
+      ),
+    });
     Logger.error(err);
-    }
-        
+    throw err;
+    
+  }
 });
 
-test("@ui @positive Crear una lista en un tablero con solo numeros", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+test("@ui @positive Crear una lista en un tablero con solo numeros", async ({
+  loginFixture,
+}) => {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
     await boardPage.clickBotonTablero();
     Logger.info("Agregando una lista con numeros");
-    await tableroPage.agregarLista('25644118489798');
+    await tableroPage.agregarLista("25644118489798");
     const confListaPage = new ListPage(page, "25644118489798");
     Logger.info("verificando que se creo en la tabla correcta");
-    await expect(page).toHaveURL("https://trello.com/b/wWp9CEFf/mi-tablero-de-trello");
-    Logger.info("Eliminando la lista")
+    await expect(page).toHaveURL(
+      "https://trello.com/b/wWp9CEFf/mi-tablero-de-trello"
+    );
+    Logger.info("Eliminando la lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Crear una lista en un tablero con solo numeros`) });
-    throw err;
+  } catch (err) {
+    await page.screenshot({
+      path: screenshotPath('Crear una lista en un tablero con solo numeros'),
+    });
     Logger.error(err);
-    }  
-        
+    throw err;
+    
+  }
 });
 
-
 test("@ui @smoke Archivar una Lista", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
     await boardPage.clickBotonTablero();
-    Logger.info("Agregando una lista para archivar")
+    Logger.info("Agregando una lista para archivar");
     await tableroPage.agregarLista("Lista de prueba Archivar");
     const confListaPage = new ListPage(page, "Lista de Prueba Archivar");
     Logger.info("Archivando la lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Archivar una Lista`) });
-    throw err;
+  } catch (err) {
+    await page.screenshot({ path: screenshotPath('Archivar una Lista') });
     Logger.error(err);
-    }
+    throw err;
+    
+  }
 });
 
-
 test("@ui @positive Contraer una Lista", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
@@ -107,19 +125,19 @@ test("@ui @positive Contraer una Lista", async ({ loginFixture }) => {
     Logger.info("Contraiendo la lista");
     await confListaPage.contraerLista();
     await confListaPage.expandirLista();
-    Logger.info("Lista archivada")
-    await confListaPage.archive(); 
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Contraer una Lista`) });
-    throw err;
+    Logger.info("Lista archivada");
+    await confListaPage.archive();
+  } catch (err) {
+    await page.screenshot({ path: screenshotPath('Contraer una Lista') });
     Logger.error(err);
-    }         
+    throw err;
+    
+  }
 });
 
-
 test("@ui @positive Editar Nombre de una Lista", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
@@ -127,22 +145,30 @@ test("@ui @positive Editar Nombre de una Lista", async ({ loginFixture }) => {
     Logger.info("Agregando una lista para editar nombre");
     await tableroPage.agregarLista("Lista de prueba para Editar");
     Logger.info("Renombrando la lista");
-    await tableroPage.renombrarLista(page,'Lista de Prueba para Editar','Renombrado');
-    Logger.info("Validando que se edito el nombre")
-    await expect(page.locator('[data-testid="list-header"]', { hasText: 'Renombrado' })).toBeVisible();
+    await tableroPage.renombrarLista(
+      page,
+      "Lista de Prueba para Editar",
+      "Renombrado"
+    );
+    Logger.info("Validando que se edito el nombre");
+    await expect(
+      page.locator('[data-testid="list-header"]', { hasText: "Renombrado" })
+    ).toBeVisible();
     const confListaPage = new ListPage(page, "Renombrado");
     Logger.info("Archivando Lista");
-    await confListaPage.archive(); 
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Editar Nombre de una Lista`) });
-    throw err;
+    await confListaPage.archive();
+  } catch (err) {
+    await page.screenshot({
+      path: screenshotPath('Editar Nombre de una Lista'),
+    });
     Logger.error(err);
-    }           
+    throw err;
+  }
 });
 
 test("@ui @positive Seguir una Lista", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try{
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
@@ -153,21 +179,22 @@ test("@ui @positive Seguir una Lista", async ({ loginFixture }) => {
     Logger.info("Siguiendo la lista");
     await confListaPage.seguirLista();
     Logger.info("Verificando que se esta siguiendo la lista");
-    const iconoOjo = page.locator('[data-testid="list"]', { hasText: 'Lista de prueba seguir' }) // el contenedor que sí tiene el nombre
-    .locator('[data-testid="SubscribeIcon"]');
+    const iconoOjo = page
+      .locator('[data-testid="list"]', { hasText: "Lista de prueba seguir" }) // el contenedor que sí tiene el nombre
+      .locator('[data-testid="SubscribeIcon"]');
     await expect(iconoOjo).toBeVisible();
     Logger.info("Archivando Lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Seguir una Lista`) });
+  } catch (err) {
+    await page.screenshot({ path: screenshotPath('Seguir una Lista') });
     throw err;
     Logger.error(err);
-    }
+  }
 });
 
 test("@ui @positive Dejar de Seguir una Lista", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
@@ -184,39 +211,47 @@ test("@ui @positive Dejar de Seguir una Lista", async ({ loginFixture }) => {
     await expect(iconoOjo).not.toBeVisible();
     Logger.info("Archivando Lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Dejar de Seguir una Lista`) });
+  } catch (err) {
+    await page.screenshot({
+      path: screenshotPath('Dejar de Seguir una Lista'),
+    });
+    Logger.error(err);
     throw err;
-    Logger.error(err);  
-    }
+    
+  }
 });
+import { faker } from '@faker-js/faker';
 
 test("@ui @positive Quitar Color de una Lista", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
     await boardPage.clickBotonTablero();
     Logger.info("Agregando una lista para quitar color");
-    await tableroPage.agregarLista("Lista de prueba");
-    const confListaPage = new ListPage(page, "Lista de prueba");
+    const titulo = faker.lorem.words(2);
+    await tableroPage.agregarLista(titulo);
+    const confListaPage = new ListPage(page, titulo);
     Logger.info("Agregando color de la lista");
-    await confListaPage.changeColor("orange");
+    await confListaPage.changeColor("orange",titulo);
     Logger.info("Cambiando el color de la lista");
     await confListaPage.quitarColorLista();
-    Looger.info("Archivando la Lista");
+    Logger.info("Archivando la Lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Quitar Color de una Lista`) });
-    throw err;
+  } catch (err) {
+    await page.screenshot({
+      path: screenshotPath('Quitar Color de una Lista'),
+    });
     Logger.error(err);
-    }
+    throw err;
+    
+  }
 });
 
 test("@ui @positive Mover una Lista", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
@@ -228,19 +263,20 @@ test("@ui @positive Mover una Lista", async ({ loginFixture }) => {
     await confListaPage.moverLista();
     const listas = page.locator('[data-testid="list-header"]');
     Logger.info("Verificando que se movio la lista");
-    await expect(listas.nth(0)).toContainText('Lista de prueba para Mover');
+    await expect(listas.nth(0)).toContainText("Lista de prueba para Mover");
     Logger.info("Archivando la lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Mover una Lista`) });
-    throw err;
+  } catch (err) {
+    await page.screenshot({ path: screenshotPath('Mover una Lista') });
     Logger.error(err);
-    }
+    throw err;
+    
+  }
 });
 
 test("@ui @positive Expandir una Lista", async ({ loginFixture }) => {
-    const page = loginFixture;
-    try {
+  const page = loginFixture;
+  try {
     const boardPage = new BoardPage(page);
     const tableroPage = new TableroPage(page);
     Logger.info("Ingresando al Tablero Mi tablero de Trello");
@@ -254,38 +290,46 @@ test("@ui @positive Expandir una Lista", async ({ loginFixture }) => {
     await confListaPage.expandirLista();
     Logger.info("Archivando la lista");
     await confListaPage.archive();
-    } catch (err) {
-    await page.screenshot({ path: screenshotPath(`Expandir una Lista`) });
-    throw err;
+  } catch (err) {
+    await page.screenshot({ path: screenshotPath('Expandir una Lista') });
     Logger.error(err);
-    }
+    throw err;
+    
+  }
 });
 
 test.describe("Agregar color a una Lista", () => {
-    for (const color of colorsData.colors) {
-        test(`@ui @positive Crea lista y aplica el Color: ${color}`, async ({ loginFixture }) => {
-        const page = loginFixture;
-        try {
+  for (const color of colorsData.colors) {
+    test("@ui @positive Crea lista y aplica el Color: ${color}", async ({
+      loginFixture,
+    }) => {
+      const page = loginFixture;
+      try {
         const boardPage = new BoardPage(page);
         const tableroPage = new TableroPage(page);
         Logger.info("Ingresando al Tablero Mi tablero de Trello");
         await boardPage.clickBotonTablero();
         Logger.info("Agregando una lista para aplicar color");
         await tableroPage.agregarLista(`Lista de coloreada ${color}`);
-        const confListaPage = new ListPage(page,`Lista de coloreada ${color}`);
+        const confListaPage = new ListPage(page, `Lista de coloreada ${color}`);
         Logger.info("Agregando color a la lista");
         await confListaPage.changeColor(color); // <-- ahora tu método recibe el color
-        const lista = page.locator('[data-testid="list"]', { hasText: `Lista de coloreada ${color}` });
+        const lista = page.locator('[data-testid="list"]', {
+          hasText: `Lista de coloreada ${color}`,
+        });
         Logger.info("Verificando que se aplico el color");
-        const styleValue = await lista.getAttribute('style');
+        const styleValue = await lista.getAttribute("style");
         expect(styleValue).toContain(`--ds-background-accent-${color}-subtler`);
         Logger.info("Archivando la lista");
         await confListaPage.archive();
-        } catch (err) {
-        await page.screenshot({ path: screenshotPath(`Crea lista y aplica el Color`) });
-        throw err;
-        Logger.error(err);
-        }
+      } catch (err) {
+        await page.screenshot({
+          path: screenshotPath("Crea lista y aplica el Color"),
         });
-    }
+        Logger.error(err);
+        throw err;
+        
+      }
+    });
+  }
 });
